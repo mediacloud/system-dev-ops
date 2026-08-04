5798629df894feb6bc9bf19d5d93871d31867c23
Phil Budne <phil@regressive.org>
2026-08-04 12:03:05 -0400

    statsd-agent.py: fix time unit confusion, pct merged, add DiskOpStats

--- monitoring/statsd-agent/statsd-agent.py
+++ monitoring/statsd-agent/statsd-agent.py
@@ -10,6 +10,7 @@ import os
 import socket
 import sys
 import time
+from typing import NamedTuple
 
 import statsd
 import psutil
@@ -40,33 +41,49 @@ def get_devices():
             dev_to_fs[devname] = disk.mountpoint
             filesystems.add(disk.mountpoint)
 
+class DiskOpStats(NamedTuple):
+    completed: int
+    merged: int
+    kB: int
+    ms: int
+
 def full_diskstats():
-    # psutil disk_io_counters is incomplete!
-    # see https://kernel-internals.org/io/observability/
-    # for descriptions
+    """
+    psutil disk_io_counters is incomplete!
+    see https://www.kernel.org/doc/Documentation/admin-guide/iostats.rst
+    """
     ret = {}
     with open("/proc/diskstats") as f:
         for line in f:
-            # docs are one-based in terms of fields after the name:
-            # 0 & 1 are major and minor device numbers?
-            toks = line.strip().split()[2:]
+            toks = line.strip().split()[2:] # discard major/minor
             device = toks[0]
             if device.startswith("loop"):
                 continue
             ret[device] = devstats = {}
-            # discard are TRIM discard ops
+            # "discard" are TRIM discard ops?
             ops = devstats["ops"] = {}
             for op, start in [("read", 1), ("write", 5), ("discard", 12)]:
-                ops[op] = {
-                    "completed": int(toks[start]),
-                    "merged": int(toks[start+1]),
-                    "kb": int(toks[start+2]) // 2, # sectors to kB
-                    "ms": int(toks[start+3])
-                }
+                ops[op] = DiskOpStats(
+                    completed=int(toks[start]), # requests completed
+                    merged=int(toks[start+1]),  # requests merged (not in compl.)
+                    kB=int(toks[start+2]) // 2, # convert sectors to kB
+                    ms=int(toks[start+3])       # total ms for all requests
+                )
             # not per disk:
             devstats["in-progress"] = int(toks[9])
-            devstats["time-busy"] = int(toks[10])
-            devstats["weighted-time"] = int(toks[11]) # qlen x time product?
+
+            # "# of milliseconds spent doing I/Os (unsigned int)
+            #  This field increases so long as field 9 is nonzero."
+            devstats["time-busy"] = int(toks[10]) # ms
+
+            # "This field is incremented at each I/O start, I/O completion, I/O
+            #  merge, or read of these stats by the number of I/Os in progress
+            #  (field 9) times the number of milliseconds spent doing I/O since the
+            #  last update of this field.  This can provide an easy measure of both
+            #  I/O completion time and the backlog that may be accumulating."
+            devstats["weighted-time"] = int(toks[11]) # in-progress x time
+
+            # not tracked for partitions:
             devstats["flush-completed"] = int(toks[16])
             devstats["flush-ms"] = int(toks[17])
     return ret
@@ -77,9 +94,10 @@ def report(f, prev, curr):
     """
     curr['time'] = now = time.monotonic()
     if prev:
-        dt = (now - prev['time']) * 1000 # ms
+        dt_s = now - prev['time']
     else:
-        dt = 0
+        dt_s = 0
+    dt_ms = dt_s * 1000
 
     for mount_point in filesystems:
         disk_usage = psutil.disk_usage(mount_point)
@@ -116,14 +134,16 @@ def report(f, prev, curr):
     # I saw Zabbix, and couldn't find anything off the shelf....
     # see full_diskstats() for generation/descriptions
 
+    # https://kernel-internals.org/io/observability/
+    # was of initial help understanding how iostat cooks its results
+
     # FINALLY doing deltas and calculations here because it's too much of a
     # pain in graphite queries (and need to know the reporting interval),
-
     # trying to stick to what "iostat -x" reports and not make
     # anything up!
     fulldisk = curr['disk'] = full_diskstats()
     prevdisk = prev.get('disk')
-    if prevdisk and dt:
+    if prevdisk and dt_s:
         for dev, fspath in dev_to_fs.items():
             fsname = DISKS.get(fspath, "")
             if not fsname:
@@ -133,45 +153,53 @@ def report(f, prev, curr):
             p = prevdisk[dev]
 
             def g(op, stat, value):
-                if value >= 0:
+                if value >= 0:  # defend against wrapping!
                     f(f"disk.nstats.{op}.{stat}.{host}.{fsname}", value)
 
             pops = p["ops"]
             for op, counts in stats["ops"].items():
                 # op is read/write/discard
-                # counts is dict with keys complete, merged, sectors, time
+                # counts is dict with keys complete, merged, kB, ms
 
                 pcounts = pops[op]      # prev counts
-                d_count = counts["completed"] - pcounts["completed"]
-                d_kb = counts["kb"] - pcounts["kb"]
-                d_ms = counts["ms"] - pcounts["ms"]
-                d_merged = counts["merged"] - pcounts["merged"]
+
+                # compute deltas (can be negative if C long wrapped):
+                d_count = counts.completed - pcounts.completed
+                d_kB = counts.kB - pcounts.kB
+                d_ms = counts.ms - pcounts.ms
+                d_merged = counts.merged - pcounts.merged
 
                 if d_count:
-                    avg_kb = d_kb / d_count
-                    pct_merged = 100 * d_merged / d_count
+                    # merged requests not included in completed total:
+                    pct_merged = 100 * d_merged / (d_count + d_merged)
+                    avg_kB = d_kB / d_count
+                    avg_wait_ms = d_ms / d_count
                 else:
-                    avg_kb = 0
-                    pct_merged = 0
+                    pct_merged = avg_kB = avg_wait_ms = 0
 
-                g(op, "reqs-sec", d_count/dt) # requests per second
-                g(op, "kb-sec", d_kb/dt) # kBbytes per second
-                g(op, "avg-wait-ms", d_ms/dt) # avg wait in ms
-                g(op, "avg-kb", avg_kb) # avg request size in kB
+                g(op, "reqs-sec", d_count / dt_s) # requests per second
+                g(op, "kb-sec", d_kB / dt_s) # kBbytes per second
+                g(op, "avg-wait-ms", avg_wait_ms) # avg wait in ms
+                g(op, "avg-kb", avg_kB) # avg request size in kB
                 g(op, "pct-merged", pct_merged) # indicates seqential access
 
-            # not operation with full stats, but using same names:
+            # not operation with full stats, but using same names as above
+            # (not available for partitions):
             d_flushes = stats["flush-completed"] - p["flush-completed"]
             d_flush_ms = stats["flush-ms"] - p["flush-ms"]
-            g("flush", "reqs-sec", d_flushes/dt) # flushes/second
-            g("flush", "avg-wait-ms", d_flush_ms/dt) # avg flush wait time
+            g("flush", "reqs-sec", d_flushes / dt_s) # flushes/second
+            if d_flushes:
+                avg_flush_ms = d_flush_ms / d_flushes
+            else:
+                avg_flush_ms = 0
+            g("flush", "avg-wait-ms", avg_flush_ms) # avg flush wait time
 
             # remainder not per-operation:
             d_weighted = stats["weighted-time"] - p["weighted-time"]
             d_busy = stats["time-busy"] - p["time-busy"]
-            g("overall", "queue-avg-len", d_weighted/dt)
-            g("overall", "in-progress", stats["in-progress"]) # instantaneous FWIW
-            g("overall", "utilization", 100*d_busy/dt)
+            g("overall", "queue-avg-len", d_weighted / dt_ms)
+            g("overall", "in-progress", stats["in-progress"]) # instantaneous
+            g("overall", "utilization", 100 * d_busy / dt_ms)
 
     cputimes = psutil.cpu_times()
     for field in cputimes._fields:
@@ -212,6 +240,8 @@ while True:
     curr = {}
     report(f, prev, curr)
     prev = curr
-   
+
+    if debug:
+        sys.stdout.flush()
     sleep_sec = INTERVAL - time.time() % INTERVAL
     time.sleep(sleep_sec)
